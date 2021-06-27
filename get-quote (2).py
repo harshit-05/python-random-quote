f46d3f7500fb4e3241bc6c924ffa11ecbed454da
harshit-05 <86473626+harshit-05@users.noreply.github.com>
2021-06-27 15:56:05 +0530

    Create get-quote (2).py

---
+++ get-quote (2).py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def main():
+    print("Keep it logically awesome.")
+
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+
+  #print(quotes)
+
+if __name__== "__main__":
+  main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
